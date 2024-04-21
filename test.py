76559c5c5cdddb262e9f03af6fcd8f7bd5740d9b
jrazzouk <janarazzouk0.com>
2024-04-21 17:58:45 +0300

    558

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 #hey , jana's talking
 print("Hello, this code is by Danah")
 #halak sameyi
+
+print("al i have to create a new branch mn vscode the i commit the changes b3d u compare and pull request b3dn u merge the branches")
