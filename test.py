9e6b75c4e64de95014321cc391a0d3bdcdb54557
danah <danahalsaghir@gmail.com>
2024-04-21 19:13:15 +0300

    final

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 #hey , jana's talking
 print("Hello, this code is by Danah")
+
 print("pushing new")
 print("changes to the repository")
+
+#halak sameyi
+print("")
+
 
 print("حبيبي يا نور العين")
